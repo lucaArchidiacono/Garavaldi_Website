af8d17852ab44f1c62884921b63049e3aa3b8123
Luca Archidiacono <lucaarchidiaconoG>
2016-11-03 17:19:30 +0100

    Added Protokoll into the project.

---
+++ app/components/arbeitsprotokoll/arbeitsProtokollController.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+    var app = angular.module('app');
+
+    app.component('arbeitsProtokoll', {
+        // Load the template
+        templateUrl: './app/components/arbeitsprotokoll/arbeitsprotokoll.html'
+    });
+})();
